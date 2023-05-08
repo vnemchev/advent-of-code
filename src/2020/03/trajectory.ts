16e3793f5133ee5a7018014067ea971f3dfc7b48
vnemchev <vnemchev@gmail.com>
2023-05-09 00:25:25 +0200

    2020 03 Refactor TS

--- src/2020/03/trajectory.ts
+++ src/2020/03/trajectory.ts
@@ -1,18 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+// Will leave these imports here for now as input is super big and ugly.
+const fs = require('fs')
+const path = require('path')
 
 const input = fs
     .readFileSync(path.resolve(__dirname, '../03/input.txt'), 'utf-8')
     .split('\n')
-    .map(e => e.trim());
+    .map((e: string) => e.trim());
 
-function trajectory(route, right, down) {
+function findTrajectory(route: string[], right: number, down: number): number {
     const tree = '#';
+    const routeL = route.length;
 
     let position = 0,
         treesHit = 0;
 
-    for (let i = 0; i < route.length; i += down) {
+    for (let i = 0; i < routeL; i += down) {
         const currentRow = route[i];
         const currentRowL = currentRow.length;
         const mIndex = currentRowL - 1;
@@ -30,10 +32,10 @@ function trajectory(route, right, down) {
     return treesHit;
 }
 
-const t1 = trajectory.bind(null, input, 1, 1),
-    t2 = trajectory.bind(null, input, 3, 1),
-    t3 = trajectory.bind(null, input, 5, 1),
-    t4 = trajectory.bind(null, input, 7, 1),
-    t5 = trajectory.bind(null, input, 1, 2);
+const t1 = findTrajectory.bind(null, input, 1, 1),
+    t2 = findTrajectory.bind(null, input, 3, 1),
+    t3 = findTrajectory.bind(null, input, 5, 1),
+    t4 = findTrajectory.bind(null, input, 7, 1),
+    t5 = findTrajectory.bind(null, input, 1, 2);
 
 console.log(`${t1() * t2() * t3() * t4() * t5()}`);
