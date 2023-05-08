4a1f9422cd19cd63768a3b5458701f796215ec7b
vnemchev <vnemchev@gmail.com>
2023-05-09 00:17:11 +0200

    Create util.ts
    Not working yet, want to make some util function to parse the input for example.

---
+++ src/util.ts
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.parseInput = (folder: string) => {
+    return fs.readFileSync(
+        path.resolve(__dirname, `../2020/${folder}/input.txt`),
+        'utf-8',
+    );
+};
