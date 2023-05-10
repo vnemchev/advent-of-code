1f33f841e8563d424f0f4110417819d782d0f1d7
vnemchev <vnemchev@gmail.com>
2023-05-10 19:12:10 +0200

    2022 05 Refactor TS

--- src/2022/05/supply-stacks.ts
+++ src/2022/05/supply-stacks.ts
@@ -1,7 +1,9 @@
-function supplyStacker(input) {
+function supplyStacker(input: string): string {
     const [stackInfo, moveInfo] = input.split('\n\n');
 
     const stack = generateStack(stackInfo);
+
+    if (typeof stack === 'string') return stack;
 
     // const modifiedStack1 = modifyStackSingle(moveInfo, stack);
     const modifiedStack2 = modifyStackMultiple(moveInfo, stack);
@@ -13,18 +15,20 @@ function supplyStacker(input) {
     return crates2;
 }
 
-function generateStack(stackInfo) {
+function generateStack(stackInfo: string): string[][] | string {
     const rows = stackInfo.split('\n');
-    const positions = rows
+    const positions: number[] | undefined = rows
         .pop()
-        .replaceAll('   ', '')
+        ?.replaceAll('   ', '')
         .trim()
         .split('')
         .map(Number);
 
+    if (positions == undefined) return 'bad input!';
+
     const posCount = positions[positions.length - 1];
-    const stack = Array.from(Array(posCount), () => []);
-    const tempStack = [];
+    const stack: string[][] = Array.from(Array(posCount), () => []);
+    const tempStack: string[][] = [];
 
     // Fill a temp array for easy storage
     for (let i = 0; i < posCount - 1; i++) {
@@ -51,21 +55,22 @@ function generateStack(stackInfo) {
     return stack;
 }
 
-function modifyStackSingle(moveInfo, stack) {
+function modifyStackSingle(moveInfo: string, stack: string[][]): string[][] {
     const rows = moveInfo.split('\n');
 
     for (let i = 0; i < rows.length; i++) {
         const [, moveCount, , from, , to] = rows[i].split(' ').map(Number);
 
         for (let j = 0; j < moveCount; j++) {
-            stack[to - 1].push(stack[from - 1].pop());
+            const takenBoxes = stack[from - 1].pop() as string;
+            stack[to - 1].push(takenBoxes);
         }
     }
 
     return stack;
 }
 
-function modifyStackMultiple(moveInfo, stack) {
+function modifyStackMultiple(moveInfo: string, stack: string[][]): string[][] {
     const rows = moveInfo.split('\n');
 
     for (let i = 0; i < rows.length; i++) {
@@ -79,7 +84,7 @@ function modifyStackMultiple(moveInfo, stack) {
     return stack;
 }
 
-function getTopCrates(stack) {
+function getTopCrates(stack: string[][]): string {
     let res = '';
 
     stack.forEach(r => (res += r[r.length - 1]));
