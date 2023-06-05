56754e2b000445595de4145cd9098d2d118c74e6
vnemchev <vnemchev@gmail.com>
2023-06-05 17:56:34 +0200

    Refactor

--- src/2022/07!/space.ts
+++ src/2022/07!/space.ts
@@ -1,103 +1,56 @@
-import { IFileTree, IDirectory } from './interfaces';
+import { IDirectory, SKELETON } from './interfaces';
 
 function main(input: string): void {
     const commands = input.split('\n');
 
-    treeCreator(commands);
+    // treeCreator(commands);
 }
 
-function treeCreator(commands: string[]): IFileTree {
-    const fileTree: IFileTree = {
-        directories: {},
-        files: {},
-    };
+function treeCreator(input: string) {
+    const commands = input.split('\n');
+
+    const fileTree: IDirectory = {};
 
     let currentDir = '';
     let prevDir = '';
 
     commands.forEach((row, i) => {
         if (row[0] == '$') {
+            ``;
             const [dollar, command, dir] = row.split(' ');
             if (command == 'ls') {
                 // TODO: Implement listing command
             } else if (command == 'cd') {
-                if (dir == '/') {
-                    fileTree.directories![dir] = {
-                        directories: {},
-                        files: {},
-                    };
+                if (dir == '/' && !fileTree[dir]) {
+                    // TODO: Implement cd + /     ----> create the object
+                    fileTree[dir] = {};
                     currentDir = dir;
                 } else if (dir == '..') {
-                    currentDir = prevDir;
+                    // TODO: Implement cd + ..    ----> go back in parent dir
+                    // prevDir = fileTree[]
                 } else {
-                    if (!fileTree.directories!.hasOwnProperty(dir)) {
-                        fileTree.directories![prevDir].directories![dir] = {
-                            directories: {},
-                            parent: currentDir,
-                        };
-                        prevDir = currentDir;
-                        currentDir = dir;
-                    }
+                    // TODO: Implement cd + name  ----> go in a new dir
+                    const mainDir = fileTree['/'];
+                    console.log(mainDir);
                 }
-
-                if (currentDir !== '/') {
-                    fileTree.directories![dir].parent = prevDir;
-                }
-                // TODO: Implement cd + /     ----> create the object
-                // TODO: Implement cd + name  ----> go in a new dir
-                // TODO: Implement cd + ..    ----> go back in parent dir
             }
         } else {
             const [dirOrFile, name] = row.split(' ');
-            if (dirOrFile == 'dir') {
-                // TODO: Implement new directory creation
-            } else if (!Number.isNaN(Number(dirOrFile))) {
-                // TODO: Implement file creation
-            }
+            // TODO: Implement new directory creation
+            // TODO: Implement file creation
         }
     });
-
     return fileTree;
 }
 
-const SKELETON: IFileTree = {
-    directories: {
-        '/': {
-            directories: {
-                a: {
-                    directories: {
-                        e: {
-                            files: { i: { size: 584, parent: 'e' } },
-                            parent: 'a',
-                        },
-                    },
-                    files: {
-                        f: { size: 29116, parent: 'a' },
-                        g: { size: 2557, parent: 'a' },
-                        'h.lst': { size: 62596, parent: 'a' },
-                    },
-                    parent: 'a',
-                },
-                d: {
-                    files: {
-                        j: { size: 4060174, parent: 'd' },
-                        'd.log': { size: 8033020, parent: 'd' },
-                        'd.ext': { size: 5626152, parent: 'd' },
-                        k: { size: 7214296, parent: 'd' },
-                    },
-                    parent: 'a',
-                },
-            },
-        },
-    },
-
-    files: {
-        'b.txt': { size: 14848514, parent: '/' },
-        'c.dat': { size: 8504156, parent: '/' },
-    },
+const readSkel = () => {
+    
+    console.log(SKELETON);
 };
 
-main(`$ cd /
+readSkel();
+
+treeCreator(`$ cd /
 $ ls
 dir a
 14848514 b.txt
